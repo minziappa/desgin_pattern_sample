ee311c28646bde0365d9096520d5c023cbdcfcd8
kim joon <minziappa@gmail.com>
2015-07-21 19:27:56 +0900

    Add Adapter Pattern

---
+++ src/main/java/io/design/pattern/adapter/Mp4Player.java
@@ -0,0 +1,17 @@
+package io.design.pattern.adapter;
+
+public class Mp4Player implements AdvancedMediaPlayer {
+
+	@Override
+	public void playVlc(String fileName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void playMp4(String fileName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/io/design/pattern/adapter/VlcPlayer.java
@@ -0,0 +1,17 @@
+package io.design.pattern.adapter;
+
+public class VlcPlayer implements AdvancedMediaPlayer {
+
+	@Override
+	public void playVlc(String fileName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void playMp4(String fileName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
