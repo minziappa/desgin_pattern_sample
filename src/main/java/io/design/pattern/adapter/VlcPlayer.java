0cbd907f5c7aa2dc2b12d5cd1c6a81b855adba08
kim joon <minziappa@gmail.com>
2015-07-22 00:19:51 +0900

    Adapter Pattern sample

--- src/main/java/io/design/pattern/adapter/VlcPlayer.java
+++ src/main/java/io/design/pattern/adapter/VlcPlayer.java
@@ -4,14 +4,12 @@ public class VlcPlayer implements AdvancedMediaPlayer {
 
 	@Override
 	public void playVlc(String fileName) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Playing vlc file. Name: "+ fileName);
 	}
 
 	@Override
 	public void playMp4(String fileName) {
-		// TODO Auto-generated method stub
-		
+		// Do nothing
 	}
 
 }
