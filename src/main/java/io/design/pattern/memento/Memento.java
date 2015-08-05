a7789b74f25b2d2a1630930c014cc8ce97c86323
kim joon <minziappa@gmail.com>
2015-08-05 19:13:44 +0900

    Add Memento Pattern

--- src/main/java/io/design/pattern/memento/Memento.java
+++ src/main/java/io/design/pattern/memento/Memento.java
@@ -1,5 +1,14 @@
 package io.design.pattern.memento;
 
 public class Memento {
+	private String state;
+
+	public Memento(String state) {
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
 
 }
